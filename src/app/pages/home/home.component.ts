3e5df8ae9c4115234f7a06421f26cdc614d422bb
guicesar2610 <guilhermedev2610@hotmail.com>
2024-03-31 09:14:14 -0300

    feat: commit with the initial project structure, configuration of eslint and prettier, and initial structuring.

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
+
+  bannerResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      this.bannerResult = result.results;
+    });
+  }
+}
